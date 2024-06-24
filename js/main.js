ff341ba6585e4c79e344ecccc8c7d9f2bf2ffb7c
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-24 14:35:48 +0200

    build: add base project files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// estrapolo il metodo del framework che ci permette di creare app Vue
+const { creatApp } = Vue;
+
+// creo l'istanza di un'app VUE e la collogo all'ID contenitore 
+creatApp ({
+    data(){
+        return {
+            
+        }
+    }
+}).mount("#app");
