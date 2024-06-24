28553b3ba5f5573b888a94cb0647734de0830dfe
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-24 14:44:30 +0200

    feat: print in page a title with data in VUE

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
 // estrapolo il metodo del framework che ci permette di creare app Vue
-const { creatApp } = Vue;
+const { createApp } = Vue;
 
 // creo l'istanza di un'app VUE e la collogo all'ID contenitore 
-creatApp ({
+createApp ({
     data(){
         return {
-            
+            message : "Titolo della pagaina"
         }
     }
 }).mount("#app");
